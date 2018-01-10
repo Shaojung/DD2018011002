a88165944b277105ecf2a37eb6513d0ef06fabaf
Teacher <shaojung@gmail.com>
2018-01-10 11:37:23 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018011002/Main2Activity.java
@@ -0,0 +1,20 @@
+package tw.com.pcschool.dd2018011002;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent it = getIntent();
+        Bundle b = it.getExtras();
+        String str = b.getString("DATA");
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(str);
+    }
+}
